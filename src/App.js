d3b9c68e9f40e3b8262357277a18670ff3b1092b
Langrangee <langrangee@000.local>
2023-08-07 16:58:16 +0100

    3rd  commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
 
 import './App.css';
+import Explore from './pages/Explore';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
+import ForgotPass from './pages/ForgotPass';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline">
-      Hello world!
-    </h1>
+   <BrowserRouter>
+          {/* Common Components */}
+          <Routes>
+            <Route path="/" element={<Explore/>}/>
+            <Route path="/sign-in" element={<SignIn/>}/>
+            <Route path="/sign-up" element={<SignUp/>}/>
+            <Route path="/profile" element={<SignIn/>}/>
+            <Route path="/offers" element={<Offers/>}/>
+            <Route path="/forgot" element={<ForgotPass/>}/>
+          </Routes>
+          {/* Navbar */}
+          <Navbar/>
+   </BrowserRouter>
   )
 }
 
